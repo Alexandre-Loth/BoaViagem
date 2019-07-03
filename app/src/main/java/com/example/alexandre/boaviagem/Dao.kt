c0b4d6c974156d0e92f96907a0bcf8ccbbd5a18c
fabiano <fabiano.oss@gmail.com>
2019-07-02 21:35:39 -0300

    Ajustes do BD

--- app/src/main/java/com/example/alexandre/boaviagem/Dao.kt
+++ app/src/main/java/com/example/alexandre/boaviagem/Dao.kt
@@ -8,8 +8,9 @@ import androidx.room.Query
 interface LoginDao {
     @Insert
     fun insert(login: Login)
-    fun findAll(): List<Login>
-    @Query("select * from login where id_login")
+
+
+    @Query("select * from login where id_login=:id")
     fun findById(id: Int): Login
 }
 
@@ -17,8 +18,11 @@ interface LoginDao {
 interface DestinoDao {
     @Insert
     fun insert(destino: Destino)
+
+    @Query("select * from destino")
     fun findAll(): List<Destino>
-    @Query("select * from destino where id_destino")
+
+    @Query("select * from destino where id_destino=:id")
     fun findById(id: Int): Destino
 }
 
@@ -26,7 +30,9 @@ interface DestinoDao {
 interface GastosDao {
     @Insert
     fun insert(gastos: Gastos)
+
+    @Query("select * from gastos")
     fun findAll(): List<Gastos>
-    @Query("select * from gastos where id_gasto")
+    @Query("select * from gastos where id_gasto=:id")
     fun findById(id: Int): Gastos
 }
